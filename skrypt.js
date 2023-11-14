a1961433ee6e18369bbbeaba2921bf7c172c21a2
Hexx333 <116751160+Hexx333@users.noreply.github.com>
2023-11-14 14:51:30 +0100

    Update skrypt.js

--- skrypt.js
+++ skrypt.js
@@ -7,10 +7,10 @@ let number = Math.floor(Math.random() = 101);
         if (userNumber > number)  {
             message = "Twój numer jest za duży"
         } else if (userNumber < number) {
-            message = "Twój numer jest za mały"
+           message = "Twój numer jest za mały"
         }else {
             message = "Gratulacje, to właściwa liczba"
         }
 
-    document.getElementById("message").innerHTML = "Gratulacje, to właściwa liczba"
+   <button id = message></button> document.getElementById("message").innerHTML = "Gratulacje, to właściwa liczba"
 }
